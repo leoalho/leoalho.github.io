f7bc35e5142e0f88bd1204bcbc66fd29da918ab3
Leo Alho <leo.a.alho@gmail.com>
2024-04-03 20:35:49 +0300

    Add text for techStack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,33 +8,7 @@ const Index = () => {
     <div>
       <div>
         Hi, I am Leo Alho, a medical doctor, full stack developer and father of
-        two. This is my developer homepage.
-      </div>
-      <div className="my-6">
-        <h2>My current preferred stack for web applications:</h2>
-        <ul className="ml-6">
-          <li>Frontend: Next.js (with Typescript), tailwind/css</li>
-          <li>Backend: Typescript, Express, socket.io</li>
-          <li>Formatting: ESLINT, Prettier</li>
-          <li>Testing: Jest (unit), Cypress (E2E)</li>
-          <li>
-            Databases: PostgreSQL (PostGIS), SQLite for small apps and
-            sideprojects
-          </li>
-          <li>CI/CD: Github actions</li>
-          <li>
-            DevOps: Digital Ocean, Ubuntu Server 22.04 LTS, Docker (Docker
-            swarm), NGINX (load balancing)
-          </li>
-          <li>UI design: Figma</li>
-        </ul>
-      </div>
-      <div>
-        <h2>I'm currently learning:</h2>
-        <ul className="ml-6">
-          <li>SASS</li>
-          <li>Playwright</li>
-        </ul>
+        two. This is my personal homepage.
       </div>
     </div>
   );
--- src/pages/techStack.tsx
+++ src/pages/techStack.tsx
@@ -1,1 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+export const config = {
+    unstable_runtimeJS: false,
+};
 
+const TechStack = () => {
+    return (
+        <div className="my-6">
+            <h2>My current preferred stack for web applications:</h2>
+            <ul className="ml-6">
+                <li>Frontend: Next.js (with Typescript), tailwind/css</li>
+                <li>Backend: Typescript, Express, socket.io</li>
+                <li>Formatting: ESLINT, Prettier</li>
+                <li>Testing: Jest (unit), Cypress (E2E)</li>
+                <li>
+                    Databases: PostgreSQL (PostGIS), SQLite for small apps and
+                    sideprojects
+                </li>
+                <li>CI/CD: Github actions</li>
+                <li>
+                    DevOps: Digital Ocean, Ubuntu Server 22.04 LTS, Docker (Docker
+                    swarm), NGINX (load balancing)
+                </li>
+                <li>UI design: Figma</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TechStack
