a2ee59a0c9f9674ad408424d3eb7f1a9d00fbfd3
Leo <leo.a.alho@gmail.com>
2025-12-11 20:53:53 +0200

    Fix plausible v2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,14 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Head from "next/head";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Leo Alho's homepage</title>
+        <script defer data-domain="alho.dev" src="https://analytics.octofy.ai/js/script.js"></script>
       </Head>
-      <Script defer data-domain="alho.dev" src="https://analytics.octofy.ai/js/script.js"/>
       <Header />
       <div className="bg-white max-w-screen-md m-auto py-6 px-2">
         <Component {...pageProps} />
