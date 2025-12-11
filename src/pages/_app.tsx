2187e958da868e32e1dfffe712e8647f0c2ec6d5
Leo <leo.a.alho@gmail.com>
2025-12-11 20:43:35 +0200

    Test plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Leo Alho's homepage</title>
       </Head>
+      <Script defer data-domain="alho.dev" src="https://analytics.octofy.ai/js/script.js"/>
       <Header />
       <div className="bg-white max-w-screen-md m-auto py-6 px-2">
         <Component {...pageProps} />
