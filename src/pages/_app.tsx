fb2f0f3291967fcea17eddadf60777dc9ecdd0bc
Leo <leo.a.alho@gmail.com>
2026-01-05 11:01:52 +0200

    Add umami tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Leo Alho's homepage</title>
         <script defer data-domain="alho.dev" src="https://analytics.octofy.ai/js/script.js"></script>
+        <script defer src="http://85.9.200.85:3113/script.js" data-website-id="cb242041-80b9-44a2-8974-5ff8f067f7f6"></script>
       </Head>
       <Header />
       <div className="bg-white max-w-screen-md m-auto py-6 px-2">
